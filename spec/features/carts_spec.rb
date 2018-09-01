62fc6256dcf572ff36d1a608934fd6a01ef2c682
Israel Tomilayo <israel.tomilayo@andela.com>
2018-09-01 11:46:17 +0100

    write more tests

--- spec/features/carts_spec.rb
+++ spec/features/carts_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Orders" do
   let!(:product) { FactoryBot.create(:product) }
+  let!(:product_2) { FactoryBot.create(:product) }
   let!(:user_order) { FactoryBot.create(:user_order)}
   let!(:cart) { FactoryBot.create(:cart)}
   let!(:delivery) { FactoryBot.create(:delivery_method) }
@@ -11,6 +12,8 @@ RSpec.describe "Orders" do
     first(".add-to-cart").click
     wait_for_ajax
     visit "/carts"
+    wait_for_ajax
+    wait_for_ajax
     expect(page.body).to have_content product.details
     expect(page.body).to have_text product.price
     expect(page.body).to have_text "Select shipping method"
@@ -26,5 +29,29 @@ RSpec.describe "Orders" do
     click_link "Make Payment"
     wait_for_ajax
     expect(current_path).to eq "/"
+  end
+
+  scenario "user can remove item from cart" do
+    visit "/"
+    first(".add-to-cart").click
+    wait_for_ajax
+    visit "/carts"
+    wait_for_ajax
+    expect(page.body).to have_content product.details
+    click_link "Remove Item"
+    visit "/carts"
+    expect(page.body).to have_content "Cart is currently empty, add products here"
+  end
+
+  scenario "user can change quantity of items in cart" do
+    visit "/"
+    first(".quick-view").click
+    fill_in("quantity", with: 2)
+    find(".quick-view-add").click
+    wait_for_ajax
+    visit "/carts"
+    expect(page.body).to have_text("Quantity:  2")
+    total_price = product.price * 2
+    expect(page.body).to have_text(number_to_currency(total_price))
   end
 end
